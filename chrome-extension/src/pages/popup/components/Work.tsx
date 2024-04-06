aff2774175710dc3f05f43d2d152d9be8d037423
Arkadiusz Mincberger <410730@student.agh.edu.pl>
2024-04-06 16:17:18 +0200

    lil clean

--- chrome-extension/src/pages/popup/components/Work.tsx
+++ chrome-extension/src/pages/popup/components/Work.tsx
@@ -5,15 +5,6 @@ import { Messages } from '../../background';
 const Work = ({
   onSend
 }) => {
-  const currentTime = (() => {
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    return `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
-  })();
-
-  const [startTime, setStartTime] = useState(currentTime)
-  const [endTime, setEndTime] = useState()
   const [percentage, setPercentage] = useState(30)
 
   return (
