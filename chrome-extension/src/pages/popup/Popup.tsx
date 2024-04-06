f9463775517b9b62944193bfa19423816556f612
szWozniak <szymek.woz@gmail.com>
2024-04-06 15:38:48 +0200

    form/work

--- chrome-extension/src/pages/popup/Popup.tsx
+++ chrome-extension/src/pages/popup/Popup.tsx
@@ -4,37 +4,25 @@ import background from '@assets/img/background.png';
 import '@pages/popup/Popup.css';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
+import Form from './components/Form';
+import Work from './components/Work';
 
 const Popup = () => {
-  const currentTime = (() => {
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    return `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
-  })();
-
-  const [startTime, setStartTime] = useState(currentTime)
-  const [endTime, setEndTime] = useState()
+  const [screen, setScreen] = useState("form")
 
   return (
     <div
       className="App"
     >
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
       <img src={background} className="App-logo" alt="logo" />
       <div className="container">
-        <span><b>Focus</b>App</span>
-        <div className="inputWithLabel">
-          <label htmlFor="startTime" className="label">Start Time</label>
-          <input id="startTime" className="timeInput" type="time" required value={startTime} onChange={e => setStartTime(e?.target?.value)} />
-        </div>
-        <div className="inputWithLabel">
-          <label htmlFor="endTime" className="label">End Time</label>
-          <input id="endTime" className="timeInput" type="time" required value={endTime} onChange={e => setEndTime(e?.target?.value)} />
-        </div>
-        <button>
-          Rozpocznij
-        </button>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+        {screen === "form" && <Form onSend={() => {
+          setScreen("work")
+        }} />}
+        {screen === "work" && <Work onSend={() => {
+          setScreen("form")
+        }} />}
         <hr />
         <button>
           <span class="material-symbols-outlined">
---
+++ chrome-extension/src/pages/popup/components/Work.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import background from '@assets/img/background.png';
+
+const Work = ({
+  onSend
+}) => {
+  const currentTime = (() => {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    return `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+  })();
+
+  const [startTime, setStartTime] = useState(currentTime)
+  const [endTime, setEndTime] = useState()
+
+  return (
+    <>
+      <span><b>Focus</b>App</span>
+      <div className="inputWithLabel">
+        <label htmlFor="startTime" className="label">Start Time</label>
+        <input id="startTime" className="timeInput" type="time" required value={startTime} onChange={e => setStartTime(e?.target?.value)} />
+      </div>
+      <div className="inputWithLabel">
+        <label htmlFor="endTime" className="label">End Time</label>
+        <input id="endTime" className="timeInput" type="time" required value={endTime} onChange={e => setEndTime(e?.target?.value)} />
+      </div>
+      <button onClick={() => onSend()}>
+        Zakończ
+      </button>
+    </>
+  );
+};
+
+export default Work
