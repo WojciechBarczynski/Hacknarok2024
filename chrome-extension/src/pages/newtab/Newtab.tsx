506ec9be33deeac55df40e4a32ceb5aa5c94cb6b
szWozniak <szymek.woz@gmail.com>
2024-04-06 19:18:34 +0200

    big changes

--- chrome-extension/src/pages/newtab/Newtab.tsx
+++ chrome-extension/src/pages/newtab/Newtab.tsx
@@ -1,66 +1,146 @@
+import { useState, useEffect } from "react"
 import withErrorBoundary from '@root/src/shared/hoc/withErrorBoundary';
 import withSuspense from '@root/src/shared/hoc/withSuspense';
 import 'react-calendar-heatmap/dist/styles.css';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import CalendarHeatmap from 'react-calendar-heatmap';
 import './Newtab.css';
+import background from '@assets/img/background.png';
+
+const today = new Date();
 
 const Newtab = () => {
-  const getRandomValuesArr = length => Array.from({ length }, () => Math.floor(Math.random() * 100) + 1);
+  const [randomValues, setRandomValues] = useState(getRange(365).map(index => {
+    return {
+      date: shiftDate(today, -index),
+      count: getRandomInt(1, 5),
+    };
+  }));
+
+  const [people, setPeople] = useState([])
+
   const getRandomProfilePic = () => `https://randomuser.me/api/portraits/men/${Math.floor(Math.random() * 100)}.jpg`;
 
-  const profilePic = getRandomProfilePic();
-  const activity = getRandomValuesArr(30);
-  const friend1Activity = getRandomValuesArr(30);
-  const friend2Activity = getRandomValuesArr(30);
 
-  const chartData = activity.map((value, index) => ({
-    date: new Date(new Date('2022-01-01').getTime() + index * 24 * 60 * 60 * 1000),
-    count: value,
-    friend1: friend1Activity[index],
-    friend2: friend2Activity[index],
-  }));
+  const [profilePic, setProfilePic] = useState(getRandomProfilePic())
 
-  const heatmapData = activity.map((value, index) => ({
-    date: new Date(new Date('2022-01-01').getTime() + index * 24 * 60 * 60 * 1000),
-    count: value,
-  }));
+  const generatePeople = (number) => {
+    for (let i = 0; i < number; i++) {
+      fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then(data => {
+          const user = data.results[0];
+          const name = `${user.name.title} ${user.name.first} ${user.name.last}`;
+          const img = user.picture.large;
+
+          setPeople(prev => [...prev, {
+            nick: name,
+            points: Math.floor(Math.random() * 100000), // Generate random points
+            img: img
+          }])
+        })
+        .catch(error => {
+          console.error('Error fetching user data:', error);
+          return null;
+        })
+    }
+  }
+
+  useEffect(() => {
+    generatePeople(10)
+  }, [])
 
   return (
     <div className="App">
-      <div className="profile">
-        <img src={profilePic} alt="Profile Picture" className="profile-pic" />
-        <div className="profile-info">
-          <h1 className="profile-name">Bjorn Ironside</h1>
-          <p className="profile-bio">Viking</p>
+      <div className="container">
+        <div className="imageBox">
+          <img src={background} className="App-logo" alt="logo" />
+          <div className="imageOverlay">
+            <div className="profile">
+              <div className="profile-pic">
+                <img src={profilePic} alt="Profile Picture" />
+              </div>
+              <div className="profile-details">
+                <div>Welcome, <b>Blondyna</b><br /></div>
+                <div className="progress-bar-labels">
+                  <div>14:00</div>
+                  <div>16:35</div>
+                </div>
+                <div className="progress-bar-container">
+                  <div
+                    className="progress-bar"
+                    style={{ width: `30%` }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="data-container">
-        <div className="chart">
-          <LineChart width={800} height={200} data={chartData}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="date" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="count" stroke="#8884d8" activeDot={{ r: 8 }} />
-            <Line type="monotone" dataKey="friend1" stroke="#82ca9d" activeDot={{ r: 8 }} />
-            <Line type="monotone" dataKey="friend2" stroke="#ffc658" activeDot={{ r: 8 }} />
-          </LineChart>
-        </div>
-        <div className="heatmap">
-          <CalendarHeatmap
-            startDate={new Date('2022-01-01')}
-            endDate={new Date('2022-01-31')}
-            values={heatmapData}
-            horizontal={false}
-            gutterSize={0.5}
-            showOutOfRangeDays={false}
-          />
+        <div className="data-container">
+          <h1>Rankings</h1>
+          <div className="rankings">
+            <div className="switches">
+              <div>Daily</div>
+              <div>Weekly</div>
+              <div>Monthly</div>
+              <div>All time</div>
+            </div>
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Friend Name</th>
+                    <th>Points</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    people.sort((a, b) => a.points < b.points ? 1 : -1).map((item, index) => <tr>
+                      <td className="profPic"><img src={item.img} alt="Profile Picture" /></td>
+                      <td>{item.nick}</td>
+                      <td>{item.points}</td>
+                    </tr>)
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <h1>Point earnings</h1>
+          <div className="heatmap">
+            <CalendarHeatmap
+              startDate={shiftDate(today, -365)}
+              endDate={today}
+              values={randomValues}
+              horizontal={true}
+              gutterSize={0.5}
+              showOutOfRangeDays={false}
+              classForValue={value => {
+                if (!value) {
+                  return 'color-empty';
+                }
+                return `color-${Math.min(value.count, 4)}`;
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
   );
 };
+
+function shiftDate(date, numDays) {
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + numDays);
+  return newDate;
+}
+
+function getRange(count) {
+  return Array.from({ length: count }, (_, i) => i);
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export default withErrorBoundary(withSuspense(Newtab, <div> Loading ... </div>), <div> Error Occur </div>);
